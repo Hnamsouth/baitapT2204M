7d6a084f20530a1c883085a9d0cca69639024c7c
hoang nam <namhvth2204008@fpt.edu.vn>
2022-05-25 15:30:22 +0700

    don ss5

--- Sesson 5/Sesson 5/B1.cpp
+++ Sesson 5/Sesson 5/B1.cpp
@@ -1,67 +1,55 @@
 #include<iostream>
 using namespace std;
+
 int main(){
     int ar[1000];
-    int n;
+    int n,x;
     cout<<"nhap so luong phan tu trong mang"<<endl;
     cin>>n;
+    int max=0,vtri=0;
     for(int i=0;i<n;i++){
         cin>>ar[i];
-        
-        if(i>=1){
-            int j=i;
-            int bk=ar[i]; 
-            while((j>0)&& (bk<ar[j-1])){  
-                ar[j]=ar[j-1];        
-                j--;
+    }
+    cout<<"nhap so nguyen x:";
+    cin>>x;
+    for(int i=0;i<n;i++){
+        int kc=0;
+        if(ar[i]<0){
+            if(ar[i]>x){
+                kc=ar[i]-x;
+                if(kc>max){
+                    max=kc;
+                    vtri=i;
+                }
+            }else{
+                kc=x-(ar[i]);
+                if(kc>max){
+                    max=kc;
+                    vtri=i;
+                }
             }
-            ar[j]=bk;                      
+            
+        }else{
+            if(ar[i]<x){
+                kc=x-ar[i];
+                if(kc>max){
+                    max=kc;
+                    vtri=i;
+                }
+            }else{
+                kc=ar[i]-x;
+                if(kc>max){
+                    max=kc;
+                    vtri=i;
+                }
+            }
         }
     }
-    int low =0,high = n;
-    int socantim,kc=0,vtri,bdem=0;
-    cout<<"nhap so can tim: "<<endl;
-    cin>>socantim;
-    do
-    {
-        int mid = (low + high )/2;
-        if(socantim<=ar[mid]){
-            if(ar[mid]<0){
-                kc=ar[mid]-socantim;
-                if(bdem==0||kc<bdem){
-                    bdem=kc;
-                    vtri=mid;
-                }
-            }else{
-                kc=socantim+ar[mid];
-                if(bdem==0||kc<bdem){
-                    bdem=kc;
-                    vtri=mid;
-                }
-            }
-            high = mid -1;
-        }else if (socantim >= ar[mid]){
-            if(ar[mid]<0){
-                kc=socantim-ar[mid];
-                if(bdem==0||kc<bdem){
-                    bdem=kc;
-                    vtri=mid;
-                }
-            }else{
-                kc=socantim+ar[mid];
-                if(bdem==0||kc<bdem){
-                    bdem=kc;
-                    vtri=mid;
-                }
-            }
-            low = mid +1;
-        }
-    }while(low!=high+1);
-        if(ar[vtri]==socantim){
-            cout<<"khong co gia tri xa nhat";
-        }else{
-            cout<<"gia tri xa x nhat la:"<<ar[vtri];
-        }
-    // }while(socantim!=1997);
+    cout << "khoang cach xa x nhat trong mang la "<<max<<endl;
+    if(ar[vtri]==x){
+        cout << "ko co gia tri nao xa nhat trong mang"<<endl;
+    }else{
+        cout<< " gia tri xa x nhat trong mang la : "<< ar[vtri]<<endl;
+    }
     return 0;
 }

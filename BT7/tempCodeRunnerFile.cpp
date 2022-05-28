a1c2391c387c19e12d6d245191b2eed46f23239f
hoang nam <namhvth2204008@fpt.edu.vn>
2022-05-28 14:24:26 +0700

    1

--- BT7/tempCodeRunnerFile.cpp
+++ BT7/tempCodeRunnerFile.cpp
@@ -1,2 +1,5 @@
-cout<<minar<<endl;
-    // cout<<ttd(minar);
+ // if(bcnn(n,ar)==0){
+    //     cout<<"mang ko co boi chung";
+    // }else{
+    //     cout<<"uoc chung nho nhat cua mang la "<<bcnn(n,ar);
+    // }
---
+++ BT7/tt.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+int main(){
+    
+    int n=5;
+    int ar[5]={-7,0,2,3,3};
+
+    int bcln;
+    
+    int i=*std::max_element(ar,ar+n);
+    for(i; ;i++){
+        int k=0;
+        for(int j=n;j>=0;j--){
+            if(ar[j]==0){
+                continue;
+            }else{
+                if(i%ar[j]!=0){
+                    k=1;
+                    break;
+                }
+            }
+        }
+        if(k==0){
+            bcln=i; 
+            break;
+        }
+    }
+    // cout<<bcln;
+    cout<<"boi chung nho nhat cua mang la: "<<bcln;
+    return 0;
+}

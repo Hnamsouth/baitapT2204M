f8c5b4b609926a0e4feceabed8c1e91633f11c90
hoang nam <namhvth2204008@fpt.edu.vn>
2022-06-02 11:18:01 +0700

    1

--- BT9/pointer.cpp
+++ BT9/pointer.cpp
@@ -1,9 +1,58 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
 int main(){
+/*
+    int arr[5]={5,9,3,5,6};
+    // int *p=&arr;
+    *(arr+1)=10; // trỏ đến vị trị trong mảng -> 5 10 3 5 6
     
+    for(int i=0;i<5;i++){
+        // cout<<*p[i];
+        cout<<*(arr+i)<<" ";
+    }
+*/
+/*
+    int *array= new int[5];
+    
+    for(int i=0;i<5;i++){
+        cin>>array[i];
+    }
+    for(int i=1;i<5;i++){
+        // cout<<*(array+i)<<" ";
+    }
+*/
+    int *ctro;
+    int max=ctro[0];
+    ctro=(int*)malloc(5*sizeof(int)); // cấp phát bộ nhớ động
+    for(int i=0;i<5;i++){
+        cin>>ctro[i];
+         if(max<ctro[i]){
+            max=ctro[i];
+        }
+    }
+    
+    cout<<"so lon nhat cuar mang 1 la: "<<max;
+    
+    ctro=(int*)realloc(ctro,15*sizeof(int)); // thay doi kick thước của mang
+    for(int i=5;i<15;i++){
+        cin>>ctro[i];
+        
+    }
+    for(int i=5;i<15;i++){
+        if(max<ctro[i]){
+            max=ctro[i];
+        }
+    }
+    cout<<"so lon nhat cuar mang 2 la: "<<max;
+    free(ctro); //giải phóng bộ nhớ đã cấp khi ko dùng đến
+    
+  
 
+
+    
 
     return 0;
 }
---
+++ BT9/struct.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct hocsinh{
+    int tuoi[10];
+    string name[10];
+    float chieu_cao[10];
+
+    void nhapTT() {
+        for(int i=0;i<3;i++){
+            cin>>tuoi[i]>>name[i]>>chieu_cao[i];
+        }
+        
+    }
+}hs,hs2;
+typedef struct ten{
+    int a;
+}snguoi1 ;
+class hsinh{
+    public:
+        void display (int t[10], string name[10],float cc[10],int n){
+            for(int i=0;i<3;i++){
+                cout<<t[i]<<endl<<name[i]<<endl<<cc[i]<<endl;
+            }
+        }
+}iDP;
+int main(){
+
+
+    hs.nhapTT();
+    // memcpy(&hs2,&hs,sizeof(hocsinh)); // hs2 =hs : sao chép các biến của struct
+    // iDP.display(hs2.tuoi,hs2.name,hs2.chieu_cao,10);
+
+    snguoi1 tenbien;
+    tenbien.a=24;
+
+
+    // cấp phát bộ nhớ động cho cấu trúc như các kld 
+    hocsinh *slhs;
+    slhs=(struct hocsinh*)malloc(3*sizeof(hocsinh));
+    for(int i=0;i<3;i++){
+        cin>>slhs->tuoi[i]>>slhs->name[i]>>slhs->chieu_cao[i];
+    }
+    for(int i=0;i<3;i++){
+        cout<<slhs->tuoi[i]<<slhs->name[i]<<slhs->chieu_cao[i];
+    }
+
+    free(slhs);
+    return 0;
+}

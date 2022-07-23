07e032162e7b63f8bab96093b68550f4fad38000
hoang nam <namhvth2204008@fpt.edu.vn>
2022-07-23 15:54:06 +0700

    1

--- Angular/T2204M/src/app/app.component.ts
+++ Angular/T2204M/src/app/app.component.ts
@@ -16,10 +16,6 @@ export class AppComponent {
   title = 'T2204M';
   ar=["sadasd",3,34,34]
   data:data1[]|undefined;
-  // cnt1:Food[]|undefined;
-  // cnt2:Food[]|undefined;
-  // cnt3:IScnt|undefined;
-
   constructor (private http:HttpClient){
     const url = 'https://foodgroup.herokuapp.com/api/menu';
     this.http.get<IScategory>(url)
@@ -27,23 +23,6 @@ export class AppComponent {
       this.data=value.data
       // console.log(this.data);
     })
-    // const cnt1= 'https://foodgroup.herokuapp.com/api/today-special';
-    // this.http.get<IScnt>(cnt1)
-    // .subscribe(vl1 =>{
-    //   this.cnt1=vl1.data;
-    //   // console.log(this.cnt1);
-    // })
-    // const cnt2= 'https://foodgroup.herokuapp.com/api/category/1';
-    // this.http.get<IScnt2>(cnt2)
-    // .subscribe(vl2 =>{
-    //   this.cnt2=vl2.data.foods;
-    //   console.log(this.cnt2);
-    // })
-    // const cnt3= 'https://foodgroup.herokuapp.com/api/today-special';
-    // this.http.get<IScnt>(cnt3)
-    // .subscribe(vl1 =>{
-    //   this.cnt3=vl1;
-    //   console.log(this.cnt3);
-    // })
+
  }
 }
